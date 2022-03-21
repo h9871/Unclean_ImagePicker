4d89ffa5522beedd0586104cb039a44c7553b105
yuhyeonjae <metalyacha@naver.com>
2022-03-22 08:54:48 +0900

    [master] 뷰 셋팅 진행

--- MultipleImagePicker/MultipleImagePicker/SelectAlbumView/SelectAlbumViewController.swift
+++ MultipleImagePicker/MultipleImagePicker/SelectAlbumView/SelectAlbumViewController.swift
@@ -9,6 +9,14 @@ import UIKit
 
 class SelectAlbumViewController: UIViewController {
     
+    // MARK: - 생성자
+    /// 뷰 생성
+    /// - Returns: 생성된 뷰
+    class func instance() -> SelectAlbumViewController {
+        let vc = SelectAlbumViewController()
+        return vc
+    }
+    
     /// 테이블 뷰
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
@@ -18,4 +26,9 @@ class SelectAlbumViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+// MARK: - ㄴ 뷰 셋팅
+extension SelectAlbumViewController {
+    
 }
